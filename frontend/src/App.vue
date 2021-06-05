4444e13720b86177ea48b9e802e8afbb5aab53ee
Ant-Sai <antsai@github.com>
2021-06-05 23:00:31 +0200

    add style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,9 @@
 <template>
-    <v-app>
-        <SHeader />
-        <SPage>
-            test SPAGE
-        </SPage>
-        <SFooter />
-    </v-app>
+    <SHeader />
+    <SPage>
+        test SPAGE
+    </SPage>
+    <SFooter />
 </template>
 
 <script lang="ts">
@@ -24,13 +22,9 @@ export default defineComponent({
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import "style";
+body{
+  background: var(--color-background);
 }
 </style>
