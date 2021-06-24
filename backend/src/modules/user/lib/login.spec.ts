33b0aa54ae5b599cfc0366e86f21aa09a5c2f2e0
CorentinMlo <corentin.millio@viacesi.fr>
2021-06-24 23:21:50 +0200

    create login and signup functions

---
+++ backend/src/modules/user/lib/login.spec.ts
@@ -0,0 +1,27 @@
+import "@/database/test";
+import * as Bcrypt from "bcryptjs";
+import * as Fake from "@/database/test/fake";
+import UserModel from "@/modules/user/model";
+import * as UserLib from "@/modules/user/lib";
+
+describe("user/lib/login", () => {
+
+    test("it should work when provided a good password", async () => {
+        const password = "1234";
+        const user = await Fake.generate(UserModel, { password: Bcrypt.hashSync(password, 8) });
+
+        const userResult = await UserLib.login(user.login, password);
+
+        expect(userResult.login).toBe(user.login);
+    });
+
+    test("it should  throw an error when provided a bad password", async () => {
+        const password = "1234";
+        const badPassword = "5678";
+        const user = await Fake.generate(UserModel, { password: Bcrypt.hashSync(password, 8) });
+
+        const userResultPromise = UserLib.login(user.login, badPassword);
+
+        await expect(userResultPromise).rejects.toThrow("[userModule][login] - password don't match");
+    });
+});
