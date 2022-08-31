bec2d06f027f8e7fa92a6ab680923f354d6b38a7
Jules Béal <j.bealleroux@gmail.com>
2022-08-31 23:48:27 +0200

    Update Layout.vue
    Correcting pr

--- frontend/src/components/pages/about/Layout.vue
+++ frontend/src/components/pages/about/Layout.vue
@@ -27,10 +27,10 @@
             <SSectionTitle>Recrutements</SSectionTitle>
             <br><br>
             <p>
-                Le Student Gaming Network est toujours à la recherche de nouveaux talents pour rejoindre l'équipe, pour cela tu peux consulter les <a :href="https://drive.google.com/drive/folders/1jLqGvmcxbGHl4OZaxnl6YaGxdjFTSBis?usp=sharing" target="_blank">fiches des postes ouverts actuellement</a> et voir si tu as envie de travailler avec nous
+                Le Student Gaming Network est toujours à la recherche de nouveaux talents pour rejoindre l'équipe, pour cela tu peux consulter les <a href="https://drive.google.com/drive/folders/1jLqGvmcxbGHl4OZaxnl6YaGxdjFTSBis?usp=sharing" target="_blank">fiches des postes ouverts actuellement</a> et voir si tu as envie de travailler avec nous
             </p>
             <p>
-                Tu peux ensuite remplir ce <a :href="https://forms.gle/uD9maAE7gnMiqWbv5" target="_blank">formulaire de candidature</a>, et nous te recontacterons pour en discuter un peu plus avec toi !
+                Tu peux ensuite remplir ce <a href="https://forms.gle/uD9maAE7gnMiqWbv5" target="_blank">formulaire de candidature</a>, et nous te recontacterons pour en discuter un peu plus avec toi !
             </p>
 
         </div>
