3b5a35fa21127dac08a9e1ebcdce0fc3e58d61ed
Corentin <corentin.millio@reboot-conseil.com>
2022-11-23 14:56:32 +0100

    fix text & newsletter vue

--- frontend/src/components/template/NewsletterSubscription.vue
+++ frontend/src/components/template/NewsletterSubscription.vue
@@ -1,98 +1,172 @@
 <template>
-    <div id="mc_embed_signup">
-        <SSectionTitle class="title">Newsletter</SSectionTitle>
-        <form action="https://sgnw.us20.list-manage.com/subscribe/post?u=80afd6afd952ad5837f61864e&amp;id=d7c0df515b&amp;f_id=00504ee6f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-            <div id="mc_embed_signup_scroll">
-                <div class="description">
-                    N'hésitez pas à vous inscrire à la newsletter pour suivre toutes les dernières actus<br>
-                    de nos assos, de nos tournois et bien plus encore !
-                </div>
-                <!-- <div class="indicates-required"><span class="asterisk">*</span> indicates required</div> -->
-                <div class="mc-field-group">
-                    <!-- <label for="mce-EMAIL">Adresse mail  <span class="asterisk">*</span></label> -->
-                    <!-- <SInput
-                        value=""
-                        name="EMAIL"
-                        type="email"
-                        class="required email"
-                        id="mce-EMAIL"
-                        title="Adresse email"
-                        required
-                    /> -->
-                    <input type="email" placeholder="Adresse mail" value="" name="EMAIL" class="required email" id="mce-EMAIL" required>
-                </div>
-                <div id="mce-responses" class="clear foot">
-                    <div class="response" id="mce-error-response" style="display:none"></div>
-                    <div class="response" id="mce-success-response" style="display:none"></div>
-                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_80afd6afd952ad5837f61864e_d7c0df515b" tabindex="-1" value=""></div>
-                <div class="optionalParent">
-                    <div class="clear foot">
-                        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
-                        <p class="brandingLogo"><a href="http://eepurl.com/h-tyb5" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_light_dtp.svg"></a></p>
+    <section class="newsletter-section">
+        <SSectionTitle class="title">
+            Newsletter
+        </SSectionTitle>
+        <div class="description">
+            N'hésitez pas à vous inscrire à notre Newsletter pour être au courant des dernières actualités :<br>
+            retrouvez les associations de la fédération, nos tournois, nos événements et bien plus encore !
+        </div>
+        <div id="mc_embed_signup">
+            <form
+                id="mc-embedded-subscribe-form"
+                action="https://sgnw.us20.list-manage.com/subscribe/post?u=80afd6afd952ad5837f61864e&amp;id=d7c0df515b&amp;f_id=00504ee6f0"
+                class="validate"
+                method="post"
+                name="mc-embedded-subscribe-form"
+                novalidate
+                target="_blank"
+            >
+                <div id="mc_embed_signup_scroll">
+                    <div class="mc-field-group">
+                        <!-- <SInput
+                            id="mce-EMAIL"
+                            class="required email"
+                            name="EMAIL"
+                            required
+                            title="Adresse email"
+                            type="email"
+                            value=""
+                        /> -->
+                        <input
+                            id="mce-EMAIL"
+                            class="required email"
+                            name="EMAIL"
+                            placeholder="Adresse mail"
+                            required
+                            type="email"
+                            value=""
+                        >
+                    </div>
+
+                    <div hidden="true">
+                        <input
+                            name="tags"
+                            type="hidden"
+                            value="45545"
+                        >
+                    </div>
+                    <div
+                        id="mce-responses"
+                        class="clear foot"
+                    >
+                        <div
+                            id="mce-error-response"
+                            class="response"
+                            style="display:none"
+                        />
+                        <div
+                            id="mce-success-response"
+                            class="response"
+                            style="display:none"
+                        />
+                    </div>    
+                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                    <div
+                        aria-hidden="true"
+                        style="position: absolute; left: -5000px;"
+                    >
+                        <input
+                            name="b_80afd6afd952ad5837f61864e_d7c0df515b"
+                            tabindex="-1"
+                            type="text"
+                            value=""
+                        >
+                    </div>
+                    <div class="optionalParent">
+                        <div class="clear foot">
+                            <input
+                                id="mc-embedded-subscribe"
+                                class="button"
+                                name="subscribe"
+                                type="submit"
+                                value="S'inscrire"
+                            >
+                            <p class="brandingLogo">
+                                <a
+                                    href="http://eepurl.com/h-tyb5"
+                                    title="Mailchimp - email marketing made easy and fun"
+                                ><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_light_dtp.svg"></a>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </form>
-    </div>
-    <link href="//cdn-images.mailchimp.com/embedcode/classic-071822.css" rel="stylesheet" type="text/css">
+            </form>
+        </div>
+        <link
+            href="//cdn-images.mailchimp.com/embedcode/classic-071822.css"
+            rel="stylesheet"
+            type="text/css"
+        >
+    </section>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from "vue";
-    import SSectionTitle from "@/components/design/SectionTitle.vue";
-    import SInput from "@/components/design/forms/Input.vue";
+import { defineComponent } from "vue";
+import SSectionTitle from "@/components/design/SectionTitle.vue";
+import SInput from "@/components/design/forms/Input.vue";
 
-    export default defineComponent({
-        name: "NewsletterSub",
-        components: { SSectionTitle, SInput },
-        mounted() {
-            let mcValidate = document.createElement('script');
-            mcValidate.setAttribute('src', '//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js');
-            document.head.appendChild(mcValidate);
-        },
-    });
+export default defineComponent({
+    name: "NewsletterSub",
+    components: { SInput, SSectionTitle },
+    mounted() {
+        let mcValidate = document.createElement("script");
+        mcValidate.setAttribute("src", "//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js");
+        document.head.appendChild(mcValidate);
+    }
+});
 </script>
 
 <style scoped lang="scss">
-	#mc_embed_signup {
+    .newsletter-section {
         padding-top: 36px;
-        clear: left; 
-        margin: 0 auto 0 auto;
-        font: 14px Helvetica, Arial, sans-serif;  
-        width: 600px;
-        text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: var(--length-gap-l);
 
-        /* Custom Css */
-
-        h2 {
-            font-weight: 200;
-            padding: 0;
-            margin: 15px 0;
-            font-size: 2.5em;
-            font-family: "Poppins", sans-serif;
+        .title {
+            text-align: center;
         }
 
         .description {
             text-align: center;
             opacity: 0.5;
-            font-weight: 400;
-            margin: 24px;
         }
 
-        .button {
-            height: 2.5em;
-            border: 2px solid var(--color-content-softer);
-            padding: 0 calc(var(--length-padding-m) - 2px);
-            background-color: var(--color-background-1);
-        }
+        #mc_embed_signup {
+            clear: left; 
+            margin: 0 auto 0 auto;
+            font: 14px Helvetica, Arial, sans-serif;  
+            width: 600px;
+            text-align: center;
 
-        .mc-field-group {
-            
-            input {
-                background: var(--color-background-1);
-                color: var(--color-primary);
+            /* Custom Css */
+
+            h2 {
+                font-weight: 200;
+                padding: 0;
+                margin: 15px 0;
+                font-size: 2.5em;
+                font-family: "Poppins", sans-serif;
+            }
+
+            .button {
+                height: 2.5em;
+                border: 2px solid var(--color-content-softer);
+                padding: 0 calc(var(--length-padding-m) - 2px);
+                background-color: var(--color-background-1);
+            }
+
+            .mc-field-group {
+                
+                input {
+                    background: var(--color-background-1);
+                    color: var(--color-primary);
+                }
             }
         }
     }
+
+	
 </style>
