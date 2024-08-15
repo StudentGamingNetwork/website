d53df86cc09d7171fee9cf042d32d463e164a764
Kotsudes <64353627+Kotsudes@users.noreply.github.com>
2024-08-15 18:02:21 +0200

    Dev-47 Ajouter un filtre sur la page fédération par région (#50)
    * Version 1 : Added sorting buttons in "Féderation"
    
    New class components :
    - components/design/Sort.vue
    
    * Version 2 : Added handling in the UI
    
    Added :
    - Reducer for handling sorted array
    - Conditional rendering for the sorted array
    
    See previous commit for the first merge request candidate
    
    * RC : Added Filters to the federtation page
    
    Added:
    - Le composant Badge permettant d'afficher des badges
    
    Modified:
    - Le composant Sort pour harmoniser son nom Sort -> SSort
    
    > Ne pas prendre en compte les deux versions (j'avais mal lu l'issue et ait donc fait un trie plutôt qu'un filtre)
    
    * Improved code quality to match overall rules
    
    In src/components/design/SBadge.vue:
    - Removed unused CSS
    - Refractored code to match overall rules
    - Simplified code inside template tags
    
    In src/components/design/SSort.vue:
    - Implemented enum for sorting
    - Refractored code to match overall rules
    - Simplified code inside template tags
    - Removed unused import
    
    In src/components/pages/federation/Search.vue:
    - Refractored code to match overall rules
    - Fixed type problems in "regionSortedAssociations"
    
    * Small refacto
    
    * Refactoring
    
    * Small refacto
    
    * Small fix
    
    * UX update
    
    * Last fix
    
    ---------
    
    Co-authored-by: Hugo ATTAL <4563971+hugoattal@users.noreply.github.com>

---
+++ frontend/src/components/design/lib/type.ts
@@ -0,0 +1,5 @@
+export enum ESortDirection {
+    DOWN = "down",
+    NONE = "none",
+    UP = "up",
+}
--- frontend/src/components/pages/federation/Search.vue
+++ frontend/src/components/pages/federation/Search.vue
@@ -7,9 +7,21 @@
                 title="Chercher une association..."
             >
                 <template #suffix>
-                    <FontAwesomeIcon :icon="['fas','search']" />
+                    <FontAwesomeIcon :icon="['fas', 'search']" />
                 </template>
             </SInput>
+            <SToggle v-model="regionDivide">
+                Région
+            </SToggle>
+        </div>
+        <div class="filter-region-wrapper">
+            <SToggle
+                v-for="key in Object.keys(visibleRegions)"
+                :key="key"
+                v-model="visibleRegions[key]"
+            >
+                {{ getRegionName(key) }}
+            </SToggle>
         </div>
         <div
             v-if="isSearching"
@@ -17,7 +29,7 @@
         >
             <FontAwesomeIcon
                 class="icon"
-                :icon="['fas','spinner']"
+                :icon="['fas', 'spinner']"
                 spin
             />
         </div>
@@ -27,29 +39,58 @@
         >
             <FontAwesomeIcon
                 class="icon"
-                :icon="['fas','frown']"
+                :icon="['fas', 'frown']"
             />
             <div class="description">
                 Aucune association trouvée...
             </div>
         </div>
-        <div class="search-result">
+        <div
+            v-if="!regionDivide"
+            class="search-result"
+        >
             <SAssociationCard
-                v-for="association in associations"
+                v-for="association in filteredAssociations"
                 :key="association._id"
                 :association="association"
             />
         </div>
+        <div
+            v-else
+            class="search-result-wrapper"
+        >
+            <template
+                v-for="(associationsEntities, region) in regionSortedAssociations"
+                :key="region"
+            >
+                <div class="title">
+                    <h2>{{ AssociationService.getRegionName(region) }}</h2>
+                </div>
+                <template v-if="visibleRegions[region as string]">
+                    <div class="search-result">
+                        <SAssociationCard
+                            v-for="association in associationsEntities"
+                            :key="association._id"
+                            :association="association"
+                        />
+                    </div>
+                </template>
+            </template>
+        </div>
     </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, onMounted, ref, watch } from "vue";
+<script lang="ts" setup>
+import { onMounted, ref, watch, computed, reactive } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { debounce } from "lodash";
 import SInput from "@/components/design/forms/Input.vue";
+import SToggle from "@/components/design/SToggle.vue";
 import SAssociationCard from "@/components/pages/federation/AssociationCard.vue";
 import * as AssociationService from "@/services/association";
+import { TAssociation } from "@/modules/association";
+import { ESortDirection } from "@/components/design/lib/type";
+import { getRegionName } from "@/services/association";
 
 type TBasicAssociation = {
     _id: string;
@@ -69,52 +110,82 @@ type TBasicAssociation = {
     school: {
         name: string;
     };
+};
+
+const isSearching = ref(true);
+const searchInput = ref("");
+const associations = ref<Array<TBasicAssociation>>([]);
+const regionDivide = ref(false);
+
+const debounceSearch = debounce(updateSearch, 500);
+
+const visibleRegions = reactive(Object.fromEntries(
+    Object.keys(AssociationService.regionNames)
+        .filter((key) => key !== "none")
+        .map((key) => [key, true])
+));
+
+const filteredAssociations = computed(() => associations.value
+    .filter((association) => visibleRegions[association.federation.region])
+);
+
+const regionSortedAssociations = computed(() => {
+    const sorted: Record<string, Array<TAssociation>> = Object.fromEntries(
+        Object.keys(AssociationService.regionNames)
+            .map((key) => [key, []])
+    );
+
+    for (const association of filteredAssociations.value) {
+        sorted[association.federation.region].push(association);
+    }
+
+    return Object.fromEntries(
+        Object.entries(sorted)
+            .filter((region) => region[1].length)
+    );
+});
+
+watch(() => searchInput.value, debounceSearch);
+
+async function updateSearch() {
+    if (associations.value.length === 0) {
+        isSearching.value = true;
+    }
+    const result = await AssociationService.search({
+        limit: 128,
+        search: searchInput.value,
+        skip: 0
+    });
+
+    associations.value = result.associations;
+    isSearching.value = false;
 }
 
-export default defineComponent({
-    name: "SFederationSearch",
-    components: { FontAwesomeIcon, SAssociationCard, SInput },
-    setup() {
-        const isSearching = ref(true);
-        const searchInput = ref("");
-
-        const associations = ref([] as Array<TBasicAssociation>);
-
-        const debounceSearch = debounce(updateSearch, 500);
-
-        watch(
-            () => searchInput.value,
-            debounceSearch
-        );
-
-        async function updateSearch() {
-            if (associations.value.length === 0) {
-                isSearching.value = true;
-            }
-            const result = await AssociationService.search({
-                limit: 128,
-                search: searchInput.value,
-                skip: 0
-            });
-
-            associations.value = result.associations;
-            isSearching.value = false;
-        }
-
-        onMounted(async () => {
-            await updateSearch();
-        });
-
-        return {
-            associations,
-            isSearching,
-            searchInput
-        };
-    }
+onMounted(async () => {
+    await updateSearch();
 });
+
 </script>
 
 <style scoped lang="scss">
+.title {
+    flex-grow: 1;
+
+    h2 {
+        font-size: 1.5rem;
+        font-weight: 600;
+        margin: 0;
+        padding: 0;
+
+        background: var(--gradient);
+        display: inline-block;
+        color: transparent;
+        background-clip: text;
+        -webkit-background-clip: text;
+        text-shadow: 0 0 16px var(--color-primary-lite);
+    }
+}
+
 .search-layout {
     display: flex;
     align-items: center;
@@ -123,8 +194,12 @@ export default defineComponent({
     gap: 64px;
 
     .search-input-wrapper {
-        max-width: 512px;
+        display: flex;
+        flex-direction: row;
+        gap: var(--length-gap-m);
+        max-width: 612px;
         width: 100%;
+        align-items: flex-end;
 
         .input {
             width: 100%;
@@ -133,9 +208,14 @@ export default defineComponent({
                 width: 100%;
             }
         }
+
+        .button {
+            border-color: var(--color-content-liter);
+        }
     }
 
-    .empty, .loading {
+    .empty,
+    .loading {
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -155,11 +235,27 @@ export default defineComponent({
         }
     }
 
+    .filter-region-wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        gap: var(--length-gap-m);
+        max-width: 980px;
+        justify-content: center;
+    }
+
     .search-result {
         display: grid;
         gap: var(--length-gap-xl);
         width: 100%;
         grid-template-columns: repeat(auto-fill, 256px);
+        justify-content: center;
+    }
+
+    .search-result-wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: var(--length-gap-xl);
+        width: 100%;
         justify-content: center;
     }
 }
