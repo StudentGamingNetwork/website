1b0a07180f35c6d44ddd027f18797e268d1ad403
Kotsudes <ridgelowao@gmail.com>
2024-09-12 18:19:59 +0200

    Added functionnality for team owners to upload team logo

---
+++ backend/src/modules/upload/handler/teamLogo.ts
@@ -0,0 +1,76 @@
+import { FastifyInstance } from "fastify";
+import { Static, Type } from "@sinclair/typebox";
+import httpErrors from "http-errors";
+import * as UploadLib from "../lib";
+import * as UserLib from "@/modules/user/lib";
+import { ERoles } from "@/modules/user/model";
+import TeamModel from "@/modules/team/model";
+
+const SchemaParams = Type.Object({
+    id: Type.String({ minLength: 1 })
+});
+
+type TSchemaParams = Static<typeof SchemaParams>;
+
+const SchemaResponse = Type.Object({
+    logo: Type.String(),
+    message: Type.String(),
+    success: Type.Boolean()
+});
+
+type TSchemaResponse = Static<typeof SchemaResponse>;
+
+const schema = {
+    params: SchemaParams,
+    response: {
+        200: SchemaResponse
+    }
+};
+
+export async function register(server: FastifyInstance): Promise<void> {
+    server.post<{ Params: TSchemaParams; Response: TSchemaResponse }>(
+        "/upload/logo/:id",
+        { schema },
+        async (request, reply) => {
+            const team = await TeamModel.findById(request.params.id);
+
+            if (!team) {
+                throw new httpErrors.NotFound("Aucune équipe trouvée.");
+            }
+
+            const user = await UserLib.getUser(request);
+
+            if (team.owner.toString() !== user.id) {
+                UserLib.assertRoles(user, [ERoles.Tournament]);
+            }
+
+            const files = await request.saveRequestFiles({
+                limits: {
+                    files: 1,
+                    fileSize: 8 * 1024 * 1024
+                }
+            });
+
+            const fileName = `${ UploadLib.generateName("logo") }.webp`;
+
+            await UploadLib.processImage(files[0], {
+                fileName,
+                path: `upload/team/${ team.id }`,
+                size: 512
+            });
+
+            if (team.settings.logo) {
+                UploadLib.deleteFile(`upload/tournament/${ team.id }/${ team.settings.logo }`);
+            }
+
+            team.settings.logo = fileName;
+            await team.save();
+
+            reply.send({
+                logo: team.settings.logo,
+                message: "Votre logo a correctement été uploadé.",
+                success: true
+            });
+        }
+    );
+}
