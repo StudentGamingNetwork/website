56e4ce6af7c6faa4787ccd6f607732c40031a666
Kotsudes <ridgelowao@gmail.com>
2024-09-19 09:36:20 +0200

    Tweaked libs used

--- backend/src/utils/database.ts
+++ backend/src/utils/database.ts
@@ -1,4 +1,4 @@
-import * as fs from "node:fs";
+import { promises as pfs } from "node:fs";
 import * as cs from "cross-spawn";
 
 const BACKUP_RETENTION_DAYS = 30;
@@ -6,17 +6,7 @@ const BACKUP_RETENTION_DAYS = 30;
 export async function dumpDatabase() {
     const dumpDate = new Date().toLocaleDateString("fr-fr",{ day: "2-digit", month: "2-digit", year: "numeric" }).replaceAll("/", "-");
     const folderName = `../dumps/${ dumpDate }`;
-    try {
-        if (!fs.existsSync("../dumps")) {
-            fs.mkdirSync("../dumps");
-        }
-        if (!fs.existsSync(folderName)) {
-            fs.mkdirSync(folderName);
-        }
-    }
-    catch (err) {
-        console.error(err);
-    }
+    await pfs.mkdir(folderName, { recursive: true });
 
     const child = cs.spawn("mongodump", ["--db", "sgnw", "--out", `../dumps/${ dumpDate }`], { stdio: "inherit" });
 }
@@ -25,19 +15,14 @@ export async function removeOldDumps() {
     const currentDate = new Date();
     const folderName = "../dumps/";
 
-    fs.readdir(folderName,{ encoding: "utf-8" }, (err, files) => {
-        if (err) 
-            console.log(err); 
-        else { 
-            files.forEach(file => { 
-                const [day, month, year] = file.split("-").map(Number);
-                const dumpDate = new Date(year, month - 1, day);
-                const Difference_In_Days = Math.floor((currentDate.getTime() - dumpDate.getTime()) / (1000 * 3600 * 24));
+    const dirs = await pfs.readdir(folderName,{ encoding: "utf-8" });
+    dirs.forEach(async (dir) => {
+        const [day, month, year] = dir.split("-").map(Number);
+        const dumpDate = new Date(year, month - 1, day);
+        const Difference_In_Days = Math.floor((currentDate.getTime() - dumpDate.getTime()) / (1000 * 3600 * 24));
 
-                if (Difference_In_Days > BACKUP_RETENTION_DAYS) {
-                    fs.rmdirSync(`${ folderName }/${ file }`, { recursive: true }); 
-                }
-            });
+        if (Difference_In_Days > BACKUP_RETENTION_DAYS) {
+            pfs.rm(`${ folderName }/${ dir }`, { recursive: true }); 
         }
     });
 }
