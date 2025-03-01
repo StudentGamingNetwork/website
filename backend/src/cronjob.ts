306af210e77347eb16da38b753a0879bdc73956f
Kotsudes <ridgelowao@gmail.com>
2025-03-01 17:38:01 +0100

    Tweaked the implementation to reflect comments

--- backend/src/cronjob.ts
+++ backend/src/cronjob.ts
@@ -3,17 +3,17 @@ import { dumpDatabase, removeOldDumps } from "@/utils/database";
 
 
 export const dumpDatabaseJob = CronJob.from({
-    start: false, 
     cronTime: "0 0 4 * * *",
     onComplete: null,
     onTick: dumpDatabase,
     timeZone: "Europe/Paris"
 });
+dumpDatabaseJob.start();
 
 export const deleteOldDumpJob = CronJob.from({
-    start: false, 
     cronTime: "0 30 4 * * *",
     onComplete: null,
     onTick: removeOldDumps,
     timeZone: "Europe/Paris"
 });
+deleteOldDumpJob.start();
--- backend/src/utils/database.ts
+++ backend/src/utils/database.ts
@@ -1,28 +1,26 @@
 import { promises as pfs } from "node:fs";
 import * as cs from "cross-spawn";
+import { format, differenceInDays, parse } from "date-fns";
+import { backupDir } from "@/utils/index";
+
 
 const BACKUP_RETENTION_DAYS = 30;
 
 export async function dumpDatabase() {
-    const dumpDate = new Date().toLocaleDateString("fr-fr",{ day: "2-digit", month: "2-digit", year: "numeric" }).replaceAll("/", "-");
-    const folderName = `../dumps/${ dumpDate }`;
-    await pfs.mkdir(folderName, { recursive: true });
-
-    const child = cs.spawn("mongodump", ["--db", "sgnw", "--out", `../dumps/${ dumpDate }`], { stdio: "inherit" });
+    const dumpDate = format(new Date(), "dd-MM-yyyy");
+    cs.spawn("mongodump", ["--db", "sgnw", "--out", `${ backupDir }/${ dumpDate }`], { stdio: "inherit" });
 }
 
 export async function removeOldDumps() {
     const currentDate = new Date();
-    const folderName = "../dumps/";
 
-    const dirs = await pfs.readdir(folderName,{ encoding: "utf-8" });
+    const dirs = await pfs.readdir(backupDir,{ encoding: "utf-8" });
     dirs.forEach(async (dir) => {
-        const [day, month, year] = dir.split("-").map(Number);
-        const dumpDate = new Date(year, month - 1, day);
-        const Difference_In_Days = Math.floor((currentDate.getTime() - dumpDate.getTime()) / (1000 * 3600 * 24));
-
-        if (Difference_In_Days > BACKUP_RETENTION_DAYS) {
-            pfs.rm(`${ folderName }/${ dir }`, { recursive: true }); 
+        const dumpDate = parse(dir, "dd-MM-yyyy", new Date());
+        const dayDifferences = differenceInDays(currentDate, dumpDate);
+        if (dayDifferences >= BACKUP_RETENTION_DAYS) {
+            
+            await pfs.rm(`${ backupDir }${ dir }`, { recursive: true }); 
         }
     });
 }
