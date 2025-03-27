8c9d88708c58a4477e5970a608a2b10ffdec2331
Kotsudes <64353627+Kotsudes@users.noreply.github.com>
2025-03-27 20:22:22 +0100

    resolve #90 (#92)
    * Fix : Timezone modification for the server
    
    * RC : Permitted the usage of the previous token for the connection

--- backend/src/modules/twoFactorAuth/lib/token.ts
+++ backend/src/modules/twoFactorAuth/lib/token.ts
@@ -2,19 +2,23 @@ import moment from "moment-timezone";
 import { authenticator } from "otplib";
 import httpErrors from "http-errors";
 
+authenticator.options = {
+    digits: 6,
+    step: 30,
+    window: [1, 0]
+};
+
 
 export function verifyToken(token: string, secret: string): boolean {
     if (!token) {
         throw new httpErrors.BadRequest("Le token est manquant.");
     }
-    
-    const epoch = moment().tz("Europe/Paris").valueOf();
-    authenticator.options = { epoch };
-    
+  
     return authenticator.check(token, secret);
 }
 
 export function generateSecret(mail: string): [string, string] {
+    
     const secret = authenticator.generateSecret();
     return [authenticator.keyuri(mail, "SGN", secret), secret];
     
